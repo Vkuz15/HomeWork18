79a9003e56fee84da89a46ac327a600bf84fbae1
Владимир <vkuznecov1504@mail.ru>
2024-04-09 21:09:24 +0400

    Написали код с помощью которого можем вывести количество дублей для каждого уникального слова

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         task1();
         task2();
         task3();
+        task4();
     }
 
     public static void task1() {
@@ -20,7 +21,7 @@ public class Main {
 
     public static void task2() {
         List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
-        Set setItems = new LinkedHashSet(nums);
+        Set<Integer> setItems = new LinkedHashSet<>(nums);
         nums.clear();
         nums.addAll(setItems);
         for (int number : nums) {
@@ -33,10 +34,25 @@ public class Main {
 
     public static void task3() {
         List<String> animals = new ArrayList<>(List.of("Лев", "Зебра", "Бобер", "Лев", "Панда", "Енот", "Панда", "Панда", "Панда", "Панда", "Скунс"));
-        Set setAnimals = new HashSet<>(animals);
+        Set<String> setAnimals = new HashSet<>(animals);
         animals.clear();
         animals.add(setAnimals.toString());
         System.out.println(animals);
+
+        System.out.println(" ");
     }
 
+    public static void task4() {
+        List<String> strings = new ArrayList<>(List.of("один", "два", "два", "три", "три", "три"));
+        Map<String, Integer> duplicates = new HashMap<>();
+        int i = 1;
+        for (String string : strings) {
+            if (duplicates.containsKey(string)) {
+                duplicates.put(string, duplicates.get(string) + 1);
+            } else {
+                duplicates.put(string, i);
+            }
+        }
+        System.out.println(duplicates);
+    }
 }
