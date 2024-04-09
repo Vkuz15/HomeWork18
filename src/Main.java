071e44440576fc885f73aa3a2f347261713d1c85
Владимир <vkuznecov1504@mail.ru>
2024-04-09 18:29:27 +0400

    Написали код с помощью которого можем вывести нечетные числа в консоль

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
+
+        List<Integer> nums = new ArrayList<>(List.of(1, 1, 2, 3, 4, 4, 5, 5, 6, 7));
+        for(int number : nums) {
+            if (number % 2 == 1) {
+                System.out.print(number + " ");
+            }
+        }
     }
 }
